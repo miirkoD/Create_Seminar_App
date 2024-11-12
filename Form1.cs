f4adb699e03a423180aaf6479715ecbf1cd6a21e
miirkoD <dabarcicm@gmail.com>
2024-11-12 10:37:02 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,11 @@
+namespace CreateSeminarApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ Utils/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CreateSeminarApp.Utils
+{
+    internal class Menu:MenuStrip
+    {
+        private Form1 mainForm;
+
+        public Menu() { 
+        
+
+            this.BackColor = ColorTranslator.FromHtml("F8BBD0");
+        }
+    }
+}
